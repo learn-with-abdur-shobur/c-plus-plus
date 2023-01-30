cbb6ab84d84de768c16802dd85bcf07a100ed946
abdur-shobur <abdurshobur.developer@gmail.com>
2023-01-30 14:00:52 +0600

    size check

---
+++ size of data types/index.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+    // size of integer 
+    int a;
+    a=10;
+    cout<< "Size of int: "<<sizeof(a)<<endl;
+
+    // size of float 
+    float b;
+    cout<< "Size of float: "<<sizeof(b)<<endl;
+
+    // size of char 
+    char c;
+    cout<< "Size of a char: "<<sizeof(c)<<endl;
+
+    // size of bool 
+    bool d;
+    cout<< "Size of a bool: "<<sizeof(d)<<endl;
+
+
+    return 0;
+}
