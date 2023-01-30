02be98c29a8230d7cfc170253677b7aa87a5d626
abdur-shobur <abdurshobur.developer@gmail.com>
2023-01-30 14:48:14 +0600

    while loop

---
+++ loops/whileLoops.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+// if given negative number will stop loop otherwise take new number. 
+int  main(){
+    int n;
+    cin >> n;
+    while ( n>0)
+    {
+        cout<<n <<endl;
+        cin>>n;
+    }
+    
+
+     return 0;
+}
