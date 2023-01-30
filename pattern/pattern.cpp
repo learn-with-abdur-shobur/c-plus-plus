2cff91b52c751775d7820383c5ffb571a8365a3e
abdur-shobur <abdurshobur.developer@gmail.com>
2023-01-30 16:04:20 +0600

    update

---
+++ pattern/pattern.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int  main(){
+    int row,col;
+    cin>>row>>col;
+
+    for (int i = 1; i <= col; i++)
+    {
+        for (int j = 1; j <= row; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+        
+    }
+    
+
+     return 0;
+}
