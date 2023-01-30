964fa88c6ef7ef709f3dfa676ff5c913fe57a575
abdur-shobur <abdurshobur.developer@gmail.com>
2023-01-30 14:17:04 +0600

    run hello world

--- hello world/hello-world.cpp
+++ hello world/hello-world.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int32_t main(){
     cout << "Hello world\n";
 
-    // help of std for run 
+    // help of std for run if you not [using namespace std]
     std::cout<<"Hello world!";
-    
+
      return 0;
 }
