de133aa949804752c2a6627a6597e7e74259984e
abdur-shobur <abdurshobur.developer@gmail.com>
2023-02-07 18:44:43 +0600

    add function

--- function/function.cpp
+++ function/function.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 void myFunction() {
-  cout << "I just got executed!";
+  cout << "I just got executed!\n";
 }
 
 int main(){
+    myFunction();
+    myFunction();
     myFunction();
     return 0;
 }
